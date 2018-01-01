2d64f2b43a86d30295e21c59574a943a879801ca
Victoria Oh <veoh1989@gmail.com>
2018-01-01 11:57:15 -0800

    fixed server side

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-'use strict';
+'use strict'
 
-// App Dependencies
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 const bodyParser = require('body-parser');
-
-// App Setup
 const app = express();
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-// Database Setup
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(DATABASE_URL);
 client.connect();
-client.on('error', err => console.error(err));
+client.on('error', console.error);
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+client.on('error', error => {
+ console.error(error);
+});
 
-app.get(‘/test’, (req, res) => res.send(‘hello world’));
-//app.all('/test', (req, res) => res.redirect(CLIENT_URL));
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/test', (req, res) => res.send('hello world'));
+
+
+app.all('*', (req, res) => res.redirect('/test'));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
